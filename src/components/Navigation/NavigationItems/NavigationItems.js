5c70e80b0bb1162c3cb64ccc3557331b3e41ee81
Frank Musick <fjmusick@hotmail.com>
2019-09-08 11:36:44 -0400

    Section 19

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,6 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/">Burger Builder</NavigationItem>
         { props.isAuth ? <NavigationItem link="/orders">Orders</NavigationItem> : null }
-        <NavigationItem link="/checkout">Checkout</NavigationItem>
         { props.isAuth ?  <NavigationItem link="/logout">Logout</NavigationItem> : <NavigationItem link="/auth">Sign In</NavigationItem> }
     </ul>
 );
